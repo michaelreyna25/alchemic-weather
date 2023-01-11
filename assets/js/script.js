05d4a3498a7fb7f3845c1ae00ae16f9815b83bc6
Michael Reyna <25.27.reyna@gmail.com>
2023-01-10 18:34:59 -0700

    added psuedo code to js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// call all element id
+
+// create let
+
+//req url geo api
+
+//req url weather api
+
+// create a function to fetch geo and weather api
+    //when the search button has been clicked
+    //then the function is called
+        //request geo location if string is = to the location
+        //
+        //call for location and day and icon
+            //create a for loop
+                //if the text
+        //display temp, humidity, and windspeed
+        //
