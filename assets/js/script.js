306b8fdef82c1c78ed3ac88b5610925de89ee66f
Michael Reyna <25.27.reyna@gmail.com>
2023-01-11 21:16:30 -0700

    added to eventlisten funct

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,43 @@
 // call all element id
-const citySearch = document.getElementById('#city-search')
-const city = document.getElementById('#city')
-const searchBttn = document.getElementById('#search-button')
-const geoLocation = document.getElementById('#geolocation')
+const citySearch = document.querySelector('#city-search')
+const city = document.querySelector('#city')
+const searchBttn = document.querySelector('#search-button')
+const geoLocation = document.querySelector('#geolocation')
 // create let
-let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=e0d0342711380951f5bd4469a6759963`
-let geographicLoc = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=1&appid=b6b2126e212f7ac9ffeaf9811e11740e`
+let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e0d0342711380951f5bd4469a6759963`
+let geographicLoc = `http://api.openweathermap.org/geo/1.0/direct?q={cityname}&limit=1&appid=b6b2126e212f7ac9ffeaf9811e11740e`
+let currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=e0d0342711380951f5bd4469a6759963`
 
-
+// let lat = $(lat);
+// let lon = $(lon);
+// let cityname = $(cityname)
 
 //req url geo api
 
 //req url weather api
-citySearch.addEventListener('submit', function(event){
+citySearch.addEventListener('click', function(event){
     event.preventDefault();
-    let lan = getElementById("lat").value;
-    let lon = getElementById("lon").value;
-    let cityName = getElementById("cityname").value
+
     localStorage.setItem("weatherUrl", weatherUrl)
     localStorage.setItem("geographicLoc", geographicLoc)
-    localStorage.setItem("lan", lan);
-    localStorage.setItem("lon", lon);
-    localStorage.setItem("cityname", cityName);
-    
+    // localStorage.setItem("lan", lan);
+    // localStorage.setItem("lon", lon);
+    // localStorage.setItem("cityname", cityName);
 
-    fetch(weatherUrl)
-        .then(function(submit){
-            return submit.json();
-        });
-        then(function(data){
-            console.log(data)
-            let 
-        })
     
-})
+   
+    fetch(currentWeather, {
+        credentials: 'same-origin',
+        redirect: 'follow',
+    })
+        .then(function(response){
+                return response.json();
+        });
+        then(function (data) {
+            console.log(data);
+        })
+    }
+);  
 // create a function to fetch geo and weather api
     //when the search button has been clicked
     //then the function is called
